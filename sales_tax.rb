eb1984bf2feaed3dd1b2612e98833b52618c8acb
MyFatherIsBatman <christianmelhuish2@gmail.com>
2014-09-11 08:55:30 -0400

    Update sales_tax.rb

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,12 +1,12 @@
 puts "What is the total amount of money spent?"
 
-total_price = gets.chomp.to_i
+      total_price = gets.chomp.to_i
 
 puts = "CALCULATING"
 
-tax = total_price * 0.0635
+      tax = total_price * 0.0635
 
-total = tax + total_price
+      total = tax + total_price
  
 puts "Your total is:"
 
